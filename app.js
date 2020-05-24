b2efff6cc28065e45f2b584b5ad2655326329aea
Akshay Bengani <akshay@rirev.com>
2020-05-24 13:07:56 +0530

    Added Whatsapp in Url

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 var express = require("express");
 var app = express();
+
+app.get('/whatsapp', function(req,res){
+    console.log(req.body);
+    res.send('Hello from Whatsapp');
+});
+
 app.get('/', function(req, res){
     res.send('hello world');
 });
